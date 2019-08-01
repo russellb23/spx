8ce2b7c7906abbb012c615697218c4b5db714fa8
russellb23 <bertrussell23@yahoo.in>
2019-08-01 15:27:29 +0530

    markov probabilities added

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,31 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
 extern crate rand;
 
 pub mod base;
 pub mod base_impl;
+pub mod markov;
 
 use std::io::stdin;
+use std::char;
+use std::f32::INFINITY;
+
 use base::Player;
 use base::Gameboard;
+use base::GameSum;
 use base::State;
 
-//use std::collections::HashMap;
+use std::collections::HashMap;
 //use std::result::Result;
 
 use rand::Rng;
 
 pub fn game(pname: String, num_rounds: usize) {
     let mut done = false;
+
+    let mut ghistory: HashMap<usize, GameSum> = HashMap::new();
 
     let mut player = Player::new(pname);
     let mut robo = Player::new(String::from("Robot"));
@@ -24,7 +35,26 @@ pub fn game(pname: String, num_rounds: usize) {
     let mut nround = 0;
     while !done {
         nround += 1;
-        let (r_daem, p_daem) = get_daemons(&mut player, &mut robo);
+        let (p_daem, r_daem) = get_daemons(&mut player, &mut robo);
+
+        if nround >= 10 && nround < num_rounds {
+            let state_prob = markovp(&ghistory, nround);
+            let rp = r_daem.get_stateid();
+            let mut cur_state: Vec<f32> = Vec::with_capacity(3);
+
+            for i in 0..3 {
+                let mut p = char::from_digit(rp as u32, 10).unwrap().to_string();
+                let c = char::from_digit(i+1 as u32, 10).unwrap().to_string();
+                p.push_str(c.as_str());
+                match state_prob.get(&p) {
+                    Some(val) => cur_state.push(*val),
+                    None => cur_state.push(INFINITY),
+                }
+            }
+            println!("r: {:.1}       \t p: {:.1}\t\t s: {:.1}\t\t [{}:{}]", 
+&cur_state[0], &cur_state[1], &cur_state[2], g.get_p_score(), g.get_r_score());
+        }
+        
         let r = r_daem.get_stateid();
         let p = p_daem.get_stateid();
 
@@ -63,16 +93,19 @@ pub fn game(pname: String, num_rounds: usize) {
             g.update_victor(Some(String::from("TIE")));
         }
 
+        ghistory.insert(nround, GameSum::new(g.clone(), vec![robo.clone(), 
+                                                             player.clone()]));
 
         if nround >= num_rounds {
             println!("{}", &g);
             done = true;
         }
     }
+
 }
 
 
-pub fn get_daemons<'a>(r: &'a mut Player, p: &'a mut Player) -> 
+fn get_daemons<'a>(p: &'a mut Player, r: &'a mut Player) -> 
                                             (&'a mut Player, &'a mut Player) { 
     let robo = rand::thread_rng().gen_range(1,4);
     let play = player_daemon();
@@ -92,7 +125,7 @@ pub fn get_daemons<'a>(r: &'a mut Player, p: &'a mut Player) ->
     (r, p)
 }
 
-pub fn player_daemon() -> State {
+fn player_daemon() -> State {
     let mut daemon = String::new();
     let mut noval = false;
 
@@ -115,6 +148,22 @@ pub fn player_daemon() -> State {
         113 => return State::Display,
         _ => { println!("\tUnrecognized choice"); player_daemon() },
     }
+}
+
+fn markovp(game_history: &HashMap<usize, GameSum>, nthround: usize) -> HashMap<String, f32> {
+    
+    let analwin: usize = 9;
+    let trange: (usize, usize) = (nthround-analwin,nthround);
+    let mut dat: Vec<usize> = Vec::with_capacity(analwin);
+    
+    for key in trange.0..trange.1 {
+        let k = game_history.get(&key);
+        let v = &k.unwrap().get_players()[0]; //1st position is robot
+        dat.push(v.get_stateid());
+    }
+
+    let pmat = markov::tranprob(&dat);
+    pmat 
 }
 
 #[cfg(test)]
